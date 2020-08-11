ada8bdfaaeabe258a19d0f7e25b2330f556c4f53
MASTRIO <56378891+MASTRIO@users.noreply.github.com>
2020-08-12 08:38:50 +1000

    uwu

--- change_display.py
+++ change_display.py
@@ -1,8 +1,6 @@
 import PySimpleGUI as sg
 import PythonChat
 
-sg.theme(Green)
-
 layout = [  [sg.Text('What is Your New Name?')],
             [sg.InputText(PythonChat.display_name,do_not_clear=True, tooltip='This is your new display name!')]
             ]
